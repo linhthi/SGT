55684542026b9dae769adb0cc1344116a4fa43c7
linhthi <hoanglinh831999@gmail.com>
2022-11-03 19:14:53 +0700

    move pre-compute features

--- data/SBMs.py
+++ data/SBMs.py
@@ -12,6 +12,7 @@ import numpy as np
 import networkx as nx
 
 import hashlib
+import precompute_features as pf
 
 
 class load_SBMsDataSetDGL(torch.utils.data.Dataset):
@@ -91,84 +92,6 @@ class SBMsDatasetDGL(torch.utils.data.Dataset):
         print("[I] Data load time: {:.4f}s".format(time.time() - start))
 
 
-def laplace_decomp(g, max_freqs):
-    # Laplacian
-    n = g.number_of_nodes()
-    A = g.adjacency_matrix_scipy(return_edge_ids=False).astype(float)
-    N = sp.diags(dgl.backend.asnumpy(g.in_degrees()).clip(1) ** -0.5, dtype=float)
-    L = sp.eye(g.number_of_nodes()) - N * A * N
-
-    # Eigenvectors with numpy
-    EigVals, EigVecs = np.linalg.eigh(L.toarray())
-    EigVals, EigVecs = EigVals[: max_freqs], EigVecs[:,
-                                             :max_freqs]  # Keep up to the maximum desired number of frequencies
-
-    # Normalize and pad EigenVectors
-    EigVecs = torch.from_numpy(EigVecs).float()
-    EigVecs = F.normalize(EigVecs, p=2, dim=1, eps=1e-12, out=None)
-
-    if n < max_freqs:
-        g.ndata['EigVecs'] = F.pad(EigVecs, (0, max_freqs - n), value=float('nan'))
-    else:
-        g.ndata['EigVecs'] = EigVecs
-
-    # Save eigenvales and pad
-    EigVals = torch.from_numpy(np.sort(np.abs(np.real(
-        EigVals))))  # Abs value is taken because numpy sometimes computes the first eigenvalue approaching 0 from the negative
-
-    if n < max_freqs:
-        EigVals = F.pad(EigVals, (0, max_freqs - n), value=float('nan')).unsqueeze(0)
-    else:
-        EigVals = EigVals.unsqueeze(0)
-
-    # Save EigVals node features
-    g.ndata['EigVals'] = EigVals.repeat(g.number_of_nodes(), 1).unsqueeze(2)
-
-    return g
-
-
-def make_full_graph(g):
-    full_g = dgl.from_networkx(nx.complete_graph(g.number_of_nodes()))
-
-    # Here we copy over the node feature data and laplace encodings
-    full_g.ndata['feat'] = g.ndata['feat']
-
-    try:
-        full_g.ndata['EigVecs'] = g.ndata['EigVecs']
-        full_g.ndata['EigVals'] = g.ndata['EigVals']
-    except:
-        pass
-
-    # Populate edge features w/ 0s
-    full_g.edata['feat'] = torch.zeros(full_g.number_of_edges(), dtype=torch.long)
-    full_g.edata['real'] = torch.zeros(full_g.number_of_edges(), dtype=torch.long)
-
-    # Copy real edge data over
-    full_g.edges[g.edges(form='uv')[0].tolist(), g.edges(form='uv')[1].tolist()].data['feat'] = torch.ones(
-        g.edata['feat'].shape[0], dtype=torch.long)
-    full_g.edges[g.edges(form='uv')[0].tolist(), g.edges(form='uv')[1].tolist()].data['real'] = torch.ones(
-        g.edata['feat'].shape[0], dtype=torch.long)
-
-    return full_g
-
-
-def add_edge_laplace_feats(g):
-    EigVals = g.ndata['EigVals'][0].flatten()
-
-    source, dest = g.find_edges(g.edges(form='eid'))
-
-    # Compute diffusion distances and Green function
-    g.edata['diff'] = torch.abs(g.nodes[source].data['EigVecs'] - g.nodes[dest].data['EigVecs']).unsqueeze(2)
-    g.edata['product'] = torch.mul(g.nodes[source].data['EigVecs'], g.nodes[dest].data['EigVecs']).unsqueeze(2)
-    g.edata['EigVals'] = EigVals.repeat(g.number_of_edges(), 1).unsqueeze(2)
-
-    # No longer need EigVecs and EigVals stored as node features
-    del g.ndata['EigVecs']
-    del g.ndata['EigVals']
-
-    return g
-
-
 class SBMsDataset(torch.utils.data.Dataset):
 
     def __init__(self, name):
@@ -197,16 +120,16 @@ class SBMsDataset(torch.utils.data.Dataset):
         return batched_graph, labels
 
     def _laplace_decomp(self, max_freqs):
-        self.train.graph_lists = [laplace_decomp(g, max_freqs) for g in self.train.graph_lists]
-        self.val.graph_lists = [laplace_decomp(g, max_freqs) for g in self.val.graph_lists]
-        self.test.graph_lists = [laplace_decomp(g, max_freqs) for g in self.test.graph_lists]
+        self.train.graph_lists = [pf.laplace_decomp(g, max_freqs) for g in self.train.graph_lists]
+        self.val.graph_lists = [pf.laplace_decomp(g, max_freqs) for g in self.val.graph_lists]
+        self.test.graph_lists = [pf.laplace_decomp(g, max_freqs) for g in self.test.graph_lists]
 
     def _make_full_graph(self):
-        self.train.graph_lists = [make_full_graph(g) for g in self.train.graph_lists]
-        self.val.graph_lists = [make_full_graph(g) for g in self.val.graph_lists]
-        self.test.graph_lists = [make_full_graph(g) for g in self.test.graph_lists]
+        self.train.graph_lists = [pf.make_full_graph(g) for g in self.train.graph_lists]
+        self.val.graph_lists = [pf.make_full_graph(g) for g in self.val.graph_lists]
+        self.test.graph_lists = [pf.make_full_graph(g) for g in self.test.graph_lists]
 
     def _add_edge_laplace_feats(self):
-        self.train.graph_lists = [add_edge_laplace_feats(g) for g in self.train.graph_lists]
-        self.val.graph_lists = [add_edge_laplace_feats(g) for g in self.val.graph_lists]
-        self.test.graph_lists = [add_edge_laplace_feats(g) for g in self.test.graph_lists]
+        self.train.graph_lists = [pf.add_edge_laplace_feats(g) for g in self.train.graph_lists]
+        self.val.graph_lists = [pf.add_edge_laplace_feats(g) for g in self.val.graph_lists]
+        self.test.graph_lists = [pf.add_edge_laplace_feats(g) for g in self.test.graph_lists]
--- data/data.py
+++ data/data.py
@@ -2,6 +2,7 @@
     File to load dataset based on user control from main file
 """
 from data.SBMs import SBMsDataset
+from dgl.data import CoraGraphDataset
 
 def load_data(dataset):
     """
@@ -13,4 +14,6 @@ def load_data(dataset):
     SBM_DATASETS = ['SBM_CLUSTER', 'SBM_PATTERN']
     if dataset in SBM_DATASETS:
         return SBMsDataset(dataset)
+    if dataset == 'CORA':
+        return CoraGraphDataset()
 
